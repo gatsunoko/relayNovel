88ebd192376643d538c636c817617ddb28f61fb2
gatsuonrails <gatsuonrails@gmail.com>
2020-05-21 20:54:31 +0900

    validates added

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -2,5 +2,13 @@ class Novel < ApplicationRecord
   belongs_to :novel_list
   belongs_to :user
 
-  validates :text, length: {maximum: 200, message: 'は200文字以内'}
+  validates :text, presence: true
+  validate :text_length
+  #通常のvalidatesで文字制限すると改行を２文字にカウントする為に独自バリデーション
+  def text_length
+    content_for_validation = text.gsub(/\r\n/,"a")
+    if content_for_validation.length > 200
+      errors.add(:text, "は200文字以内で入力してください。")
+    end
+  end
 end
